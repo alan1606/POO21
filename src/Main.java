5d0dc74c42aa082f503aa5be9364606f2e7f7049
alan1606 <62415210+alan1606@users.noreply.github.com>
2023-02-23 10:56:41 -0600

    Herencia

--- src/Main.java
+++ src/Main.java
@@ -1,37 +1,51 @@
-import java.time.LocalDate;
+class Persona2{
+	String nombre;
+	String apellido;
+	String curp;
+	
+	void respirar() {
+		System.out.println("La persona respira");
+	}
+	
+	void comer() {
+		System.out.println("La persona come");
+	}
+	
+	void dormir() {
+		System.out.println("La persona duerme");
+	}
+}
+
+
+class Alumno extends Persona2{
+	Integer estres;
+	String matricula;
+	Integer suenio;
+	
+	void estudiar() {
+		System.out.println("El alumno estudia");
+	}
+	
+	void hacerTarea() {
+		System.out.println("El alumno hace la tarea");
+	}
+}
+
+class Profesor extends Persona2{
+	Integer amargura;
+	Double salario;
+	
+	void hacerSufrir() {
+		System.out.println("El profesor da clases");
+	}
+}
 
 public class Main {
-	public void main(String[] args) {
-		// Creación de objeto
-		Persona persona1 = new Persona();
-
-		persona1.altura = 50d;
-		persona1.nombre = "Medio";
-		persona1.apellidos = "Metro";
-		persona1.curp = "ASDA000416HMXDAS3";
-		persona1.edad = 7;
-		persona1.fechaDeNacimiento = LocalDate.now();
-		persona1.imc = 15d;
-		persona1.peso = 30d;
-		persona1.rfc = "ASDA000416HMX122";
-
-		persona1.caminar();
-		persona1.comer();
-		persona1.tramites();
-//---------------------------------------------------------------------
-		Persona persona2 = new Persona();
-
-		persona2.altura = 178d;
-		persona2.nombre = "Pirata";
-		persona2.apellidos = "DJ";
-		persona2.curp = "GEXA000104HGDZP2";
-		persona2.edad = 50;
-		persona2.fechaDeNacimiento = LocalDate.now();
-		persona2.imc = 36d;
-		persona2.peso = 82d;
-		persona2.rfc = "a";
-
+	public static void main(String[] args) {
+		Persona2 persona = new Persona2();
+		Alumno alumno = new Alumno();
+		Profesor profesor = new Profesor();
 		
-		persona1.pelear(persona2);
+		alumno.respirar();
 	}
 }
