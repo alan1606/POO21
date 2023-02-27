5bff78f36ba693b7efe417ea96b62ea0a2158581
alan1606 <62415210+alan1606@users.noreply.github.com>
2023-02-27 14:01:45 -0600

    Constructores y sobreescritura
    Llamada a métodos del padre con super, sobreescritura con override

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,14 @@ class Persona2{
 	String apellido;
 	String curp;
 	
+	Persona2(){	}
+	
+	Persona2(String nombrePersona, String apellido, String curp) {
+		this.nombre = nombrePersona;
+		this.apellido = apellido;
+		this.curp = curp;
+	}
+
 	void respirar() {
 		System.out.println("La persona respira");
 	}
@@ -22,6 +30,24 @@ class Alumno extends Persona2{
 	String matricula;
 	Integer suenio;
 	
+	Alumno(){}
+
+	Alumno(String nombre,
+			String apellido,
+			String curp,
+			Integer estres, 
+			String matricula, 
+			Integer suenio){
+		
+		super(nombre, apellido, curp);
+		
+		this.estres = estres;
+		this.matricula = matricula;
+		this.suenio=suenio;
+		
+	}
+
+
 	void estudiar() {
 		System.out.println("El alumno estudia");
 	}
@@ -29,11 +55,48 @@ class Alumno extends Persona2{
 	void hacerTarea() {
 		System.out.println("El alumno hace la tarea");
 	}
+	
+	//Estoy en la clase Alumno
+	
+	
+	@Override
+	public String toString() {
+		return String.format("Nombre: %s"
+				+ "\nApellido: %s"
+				+ "\nCurp: %s"
+				+ "\nEstrés: %s"
+				+ "\nMatrícula: %s"
+				+ "\nSueño: %s",
+				nombre,
+				apellido,
+				curp,
+				estres,
+				matricula,
+				suenio
+				);
+	}
+
+	
+	
 }
 
 class Profesor extends Persona2{
 	Integer amargura;
 	Double salario;
+	
+	Profesor(){}
+	
+	Profesor(String nombre,
+			String apellido,
+			String curp,
+			Integer amargura, 
+			Double salario){	
+		
+		super(nombre, apellido, curp);
+		this.amargura = amargura;
+		this.salario = salario;
+		
+	}
 	
 	void hacerSufrir() {
 		System.out.println("El profesor da clases");
@@ -43,9 +106,22 @@ class Profesor extends Persona2{
 public class Main {
 	public static void main(String[] args) {
 		Persona2 persona = new Persona2();
-		Alumno alumno = new Alumno();
+		Alumno alumno = new Alumno
+				("alan", "aguirre","CURP", 10,"12312", 10);
 		Profesor profesor = new Profesor();
 		
 		alumno.respirar();
+		
+		boolean instanciaDePersona = persona instanceof Persona2;
+		System.out.println("Instancia de persona?: " + instanciaDePersona);
+		
+		boolean alumnoEsPersona = alumno instanceof Persona2;
+		System.out.println("Alumno es persona?: " + alumnoEsPersona);
+		
+		boolean profesorEsPersona = profesor instanceof Persona2;
+		System.out.println("Profesor es persona?: " + profesorEsPersona);
+		
+		System.out.println(alumno);
+		
 	}
 }
